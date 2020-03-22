231f2119ae18f60470e0fed6bd00753239dd1f30
Bowen Shen <bshen36@gwu,edu>
2020-03-22 15:13:12 -0400

    welcome.html SayHelloControler added

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@RequestMapping("/")
+	@RequestMapping({"/greetings"})
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
