30b995ccd7eecbf5414d60772557f91dcbca0dca
Bowen Shen <bshen36@gwu,edu>
2020-03-24 15:23:03 -0400

    small changes

--- src/main/resources/sql/people.sql
+++ src/main/resources/sql/people.sql
@@ -9,9 +9,9 @@ address varchar(150),
 zipcode integer
 );
 
-SELECT * FROM people;
+--SELECT * FROM people;
 
-INSERT INTO people(id, full_name, first_name, last_name, age, email, address, zipcode) values
-(1, 'Bowen Shen', 'Bowen', 'Shen', 24, 'bshen36@gwu.edu', '5597 Seminary Rd', 22041 ),
-(2, 'Ronny Chow', 'Ronny', 'Chow', 25, 'ronny@gwu.edu', '1900 S Eads St', 22202),
-(3, 'April Chen', 'April', 'Chen', 21, 'april@gwu.edu', '1900 S Eads St', 22202);
+-- INSERT INTO people(id, full_name, first_name, last_name, age, email, address, zipcode) values
+-- (1, 'Bowen Shen', 'Bowen', 'Shen', 24, 'bshen36@gwu.edu', '5597 Seminary Rd', 22041 ),
+-- (2, 'Ronny Chow', 'Ronny', 'Chow', 25, 'ronny@gwu.edu', '1900 S Eads St', 22202),
+-- (3, 'April Chen', 'April', 'Chen', 21, 'april@gwu.edu', '1900 S Eads St', 22202);
