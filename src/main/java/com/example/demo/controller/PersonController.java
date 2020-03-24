ed292410bd4b54b5eb28950b13adae43191046d7
Bowen Shen <bshen36@gwu,edu>
2020-03-24 15:24:10 -0400

    PersonController listAllPeople method added

---
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.service.PersonService;
+
+@Controller
+public class PersonController {
+	
+	@Autowired
+	private PersonService personService;
+	
+	@RequestMapping("/people")
+	public String listAllPeople(Model model) {
+		
+		model.addAttribute("people", personService.getAllPersons());
+		
+		return "people";
+	}
+	
+}
