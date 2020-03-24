a60b8cc65a9887b4e48119256248d3570827748e
Bowen Shen <bshen36@gwu,edu>
2020-03-23 23:41:18 -0400

    PersonDao and PersonDaoImpl added

--- src/main/java/com/example/demo/dao/PersonDao.java
+++ src/main/java/com/example/demo/dao/PersonDao.java
@@ -1,5 +1,18 @@
 package com.example.demo.dao;
 
+import java.util.List;
+
+import com.example.demo.model.Person;
+
 public interface PersonDao {
+	Person getPersonById(long id);
 	
+	List<Person> getAllPersons();
+
+	boolean createPerson(Person person);
+	
+	boolean deletePerson(Person person);
+
+	boolean updatePerson(Person person);
+
 }
--- src/main/java/com/example/demo/dao/PersonDaoImpl.java
+++ src/main/java/com/example/demo/dao/PersonDaoImpl.java
@@ -1,5 +1,51 @@
 package com.example.demo.dao;
 
-public class PersonDaoImpl {
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.model.Person;
+import com.example.demo.model.PersonMapper;
+
+@Component
+public class PersonDaoImpl implements PersonDao {
 	
+	JdbcTemplate jdbcTemplate;
+	
+	private final String SQL_FIND_PERSON = "select * from people where id = ?";
+	private final String SQL_DELETE_PERSON = "delete from people where id = ?";
+	private final String SQL_UPDATE_PERSON = "update people set full_name = ?, first_name = ?, last_name = ?, age  = ?, email = ?, address = ?, zipcode = ? where id = ?";
+	private final String SQL_GET_ALL = "select * from people";
+	private final String SQL_INSERT_PERSON = "insert into people(id, full_name, first_name, last_name, age, email, address, zipcode) values(?,?,?,?,?,?,?)";
+	
+	@Autowired
+	public PersonDaoImpl(DataSource dataSource) {
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
+	public Person getPersonById(long id) {
+		return jdbcTemplate.queryForObject(SQL_FIND_PERSON, new Object[] { id }, new PersonMapper());
+	}
+
+	public List<Person> getAllPersons() {
+		return jdbcTemplate.query(SQL_GET_ALL, new PersonMapper());
+	}
+
+	public boolean deletePerson(Person person) {
+		return jdbcTemplate.update(SQL_DELETE_PERSON, person.getId()) > 0;
+	}
+
+	public boolean updatePerson(Person person) {
+		return jdbcTemplate.update(SQL_UPDATE_PERSON, person.getFirstName(), person.getLastName(), person.getAge(),
+				person.getId()) > 0;
+	}
+
+	public boolean createPerson(Person person) {
+		return jdbcTemplate.update(SQL_INSERT_PERSON, person.getId(), person.getFirstName(), person.getLastName(),
+				person.getAge()) > 0;
+	}
 }
