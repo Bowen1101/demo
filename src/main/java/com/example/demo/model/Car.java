476d32f8c07a98556cd30917b992855f5c24d71d
Bowen Shen <bshen36@gwu,edu>
2020-03-26 22:36:10 -0400

    Car model added

---
+++ src/main/java/com/example/demo/model/Car.java
@@ -0,0 +1,45 @@
+package com.example.demo.model;
+
+public class Car {
+	
+	private long id;
+	private String vinNumber;
+	private String make;
+	private String model;
+	private String color;
+	
+	public long getId() {
+		return id;
+	}
+	public String getVinNumber() {
+		return vinNumber;
+	}
+	public String getMake() {
+		return make;
+	}
+	public String getModel() {
+		return model;
+	}
+	public String getColor() {
+		return color;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	public void setVinNumber(String vinNumber) {
+		this.vinNumber = vinNumber;
+	}
+	public void setMake(String make) {
+		this.make = make;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/example/demo/model/CarMapper.java
@@ -0,0 +1,20 @@
+package com.example.demo.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class CarMapper implements RowMapper<Car>{
+	public Car mapRow(ResultSet resultSet, int i) throws SQLException{
+		
+		Car car = new Car();
+		car.setId(resultSet.getLong("id"));
+		car.setVinNumber(resultSet.getString("vin_number"));
+		car.setMake(resultSet.getString("make"));
+		car.setMake(resultSet.getString("model"));
+		car.setMake(resultSet.getString("color"));
+		return car;
+	}
+	
+}
